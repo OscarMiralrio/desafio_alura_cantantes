606d4381cf1eb8eb79891edd71354a5e44f6c884
Oscar Sanchez M <skrsm_12@hotmail.com>
2024-05-31 21:17:41 -0600

    carga inicial

---
+++ src/main/java/com/aluracursos/artist/model/Album.java
@@ -0,0 +1,9 @@
+package com.aluracursos.artist.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "album")
+public class Album {
+}
---
+++ src/main/java/com/aluracursos/artist/model/Cantante.java
@@ -0,0 +1,12 @@
+package com.aluracursos.artist.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cantante")
+public class Cantante {
+
+
+
+}
